9bfd910adeea1fb01a91499ee00480345daea175
cristianvasiliu <cristian.adrian.vasiliu@gmail.com>
2023-08-23 18:47:41 +0300

    updated 1st test to use WireMock cloud endpoint

--- Tests/test_sample.py
+++ Tests/test_sample.py
@@ -9,9 +9,9 @@ from snapyrest.images import Img
 @pytest.mark.regression  # Custom pytest marker to run the test cases with ease on demand
 @pytest.mark.snap  # Custom pytest marker to run the test cases with ease on demand
 def test_sample_get_request_001():
-    Api.get("/name")
+    Api.get(endpoint="/valoare/1")
     Api.verify_response_code(200)
-    Api.ignore_keys("age")
+    # Api.ignore_keys("age")
     Api.verify_response_json("sample.yml", "test_sample_get_request_001")
 
 
